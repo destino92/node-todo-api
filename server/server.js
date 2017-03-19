f86f4ed1bc67a06425c6a3d33c58d807d8ffcf1d
destino92 <destinodello@gmail.com>
2017-03-19 18:59:26 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -27,6 +29,33 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET todos/1234234
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // validate id using isvalide
+  if(!ObjectID.isValid(id)){
+    // return 404 - send back empty send
+    return res.status(404).send();
+  }
+
+  // findById
+  Todo.findById(id).then((todo) => {
+    // success
+      // if no todo - send back 404 with empty body
+      if(!todo) {
+        return res.status(404).send();
+      }
+      // if todo - send it back
+      res.status(200).send({todo});
+  }).catch((e) => {
+    // error
+      // 400 - and send empty body back
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {
