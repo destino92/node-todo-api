da2340760e95b5fdaad25ba436d0b8c60a326f1f
destino92 <destinodello@gmail.com>
2017-03-28 20:24:14 +0300

    feat: Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findBytoken = function (token) {
   var User = this;
   var decoded;
